c4d5c5f5b1256a9d556068166fb9fb6015f3dc4c
Tommy Mattila <tommy.mattila@iki.fi>
2012-07-01 21:36:16 +0300

    Added assetic example use.

--- src/Lupo/Discgolf/Resources/views/Course/getCourses.html.php
+++ src/Lupo/Discgolf/Resources/views/Course/getCourses.html.php
@@ -1,3 +1,7 @@
+<?php foreach ($view['assetic']->javascripts(
+    array('@LupoDiscgolf/Resources/public/js/*')) as $url): ?>
+<script type="text/javascript" src="<?php echo $view->escape($url) ?>"></script>
+<?php endforeach; ?>
 <pre>
 <?php
 foreach ($courses as $course) {
